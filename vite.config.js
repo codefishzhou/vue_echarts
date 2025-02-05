03be2b39b222175687c429bcaa9c2c16f62be050
codefish <2358514288@qq.com>
2025-02-05 21:00:06 +0800

    // .

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   assetsPublicPath: "./",
-  base: '/echarts/',
+  base: '/vue_echarts/',
   publicPath: "./",
   resolve: {
     alias: {
